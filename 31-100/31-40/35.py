25392c533b1a7ddb468e96239d14668339fc0114
higee <gshock94@gmail.com>
2018-12-25 13:15:53 +0100

    Circular primes

---
+++ 31-100/31-40/35.py
@@ -0,0 +1,51 @@
+def prime(n):
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+def check_circular_prime(n):
+
+    str_n = str(n)
+    # create a list of rotated numbers
+    rotated_numbers = {int(str_n[i:]+str_n[:i]) for i, _ in enumerate(str_n)}
+
+    # when all rotated_numbers are prime numbers
+    if all(prime(n) for n in rotated_numbers):
+        
+        result = {
+            'status' : True,
+            'total' : len(rotated_numbers),
+            'numbers' : rotated_numbers
+        }
+
+    else:
+        result = {
+            'status' : False,
+            'numbers' : rotated_numbers
+        }
+
+    return result
+
+def main():
+    cnt = 0
+    check_list = set()
+
+    for i in range(2, 1000000):
+
+        if i in check_list:
+            continue
+
+        outcome = check_circular_prime(i)
+        # if input number is a cicular prime
+        if outcome['status']:
+            cnt += outcome['total']
+        # add rotated numbers to check-list so that we don't have to compute again
+        # add both circular and non-circular prime numbers
+        # because they are the same in logic
+        check_list.update(outcome['numbers'])
+    
+    return cnt
+
+if __name__ == "__main__":
+    print(main())
